33d48794ec4853ea0fe84fd2cc752c2f6dedfc49
Joseph Willem Ricci <josephwillemricci@gmail.com>
2023-09-09 22:23:50 -0400

    processing done(..?)

--- processor.py
+++ processor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 g_co2_per_bottle = 0.1825  # lbs of co2
+lbs_of_food_consumed_per_year = 2000
 
 # download latest results from google sheets
 # response = requests.get("https://docs.google.com/spreadsheets/d/1lkhS5Cd32grN9zt36ejl-ndQSt3yKEx1aLhh-eBPBtk/")
@@ -15,46 +16,53 @@ co2_by_food = pd.read_csv("co2_by_food.csv")  # lbs co2 per lb product
 
 num_results = len(survey_results_df)
 
-# 1
+# electricty efficienty used for calculating housing emissions
 state = survey_results_df['What state are you from?'][num_results-1]
 user_state_emissions = state_co2_df["avg_co2"][state_co2_df["state"] == state]
-user_state_electricity = co2_by_electrical_grid["lb/mwh"][co2_by_electrical_grid["state"] == state] #calc
+user_state_electricity = co2_by_electrical_grid["lb/mwh"][co2_by_electrical_grid["state"] == state] 
 
-# # 2
+# food emissions
 foods = survey_results_df['What food products make up your diet?'][num_results-1] 
 foods = foods.split(", ")
 foods_co2 = 0 #calc
 for food in foods:
     foods_co2 += co2_by_food["Total_emissions"][co2_by_food["Food product"] == food].values[0]
+foods_co2 = foods_co2 * lbs_of_food_consumed_per_year
 
-# # 3
-transportation = survey_results_df['How do you get around?'][num_results-1]
-user_transportation = survey_results_df["How do you get around?"] 
-
-# # 4
+# used for calculating car emissions
 car_year = survey_results_df['If you drive what year was your car made?'][num_results-1]
 
-# # 5
+# car emissions
 miles_per_year = survey_results_df['If you drive how many miles a year on average?'][num_results-1]
 if miles_per_year == "0-1000":
     miles_per_year = 500
 elif miles_per_year == "1000-15,000":
     miles_per_year = 8000
+elif miles_per_year == "I do not drive":
+    miles_per_year = 0
 else:
     miles_per_year = 20000
 
 car_ch4 = miles_per_year * vehicle_emmisions_by_year["ch4"][vehicle_emmisions_by_year["year"] == car_year].values[0]
 car_n2o = miles_per_year * vehicle_emmisions_by_year["n2o"][vehicle_emmisions_by_year["year"] == car_year].values[0]
-car_emissions = car_ch4 + car_n2o #calc
+car_emissions = car_ch4 + car_n2o
 
-# # 6
+# housing emissions
 housing = survey_results_df['What is your housing situation?'][num_results-1]
-user_housing = survey_results_df["What is your housing situation?"]
+if housing == "Student Dorm":
+    housing_electricity_usage = 1  #mwh
+if housing == "Apartment":
+    housing_electricity_usage = 3  #mwh
+if housing == "House":
+    housing_electricity_usage = 13  #mwh
 
-# #7
+housing_emmisions = user_state_electricity * housing_electricity_usage #calc
+
+# water emissions
 water = survey_results_df['Do you [mostly] drink from a refillable Water Bottle or drink bottled water?'][num_results-1]
-user_water_emissions = survey_results_df["Do you [mostly] drink from a refillable Water Bottle or drink bottled water?"] #calc
+if water == "Refillable Bottle":
+    water_emissions = 0
+else:
+    water_emissions = 156 * g_co2_per_bottle  # 156 bottles per year
 
-# processing
-
-total_emissions = 0
+total_emissions = foods_co2 + car_emissions + housing_emmisions + water_emissions
